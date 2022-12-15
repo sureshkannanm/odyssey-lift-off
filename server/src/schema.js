f413278b5f03ba52849ae6493174d5814f2193f5
Sureshkannan <sureshkannan.muthusamy@aa.com>
2022-12-15 13:41:46 -0500

    Initial commit

---
+++ server/src/schema.js
@@ -0,0 +1,37 @@
+const {gql} = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    "Get Tracks for home page"
+    tracksForHome: [Track!]!
+  }
+
+  """
+  A track is group of module
+  """
+  type Track {
+    "tracks id "
+    id: ID!
+    "tracks tittle "
+    title: String!
+    "tracks Author "
+    author: Author!
+    "tracks thumbnail "
+    thumbnail: String
+    "tracks duration "
+    length: Int
+    "tracks module count "
+    modulesCount: Int
+  }
+
+  """
+  Author of a complete Track
+  """
+  type Author {
+    id: ID!
+    name: String!
+    photo: String
+  }
+`;
+
+module.exports = typeDefs;
