7a4d6c59222a7328a2f5cbefa6bc2e5a3db4374e
Sureshkannan <8393993+sureshkannanm@users.noreply.github.com>
2022-12-21 21:40:39 -0500

    adding mutations

--- server/src/resolver.js
+++ server/src/resolver.js
@@ -15,6 +15,27 @@ const resolvers = {
       return dataSources.trackAPI.getTrack(id);
     },
   },
+  Mutation: {
+    // increments a track's numberOfViews property
+    incrementTrackViews: async (_, {id}, {dataSources}) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track
+        };
+      } catch (err) {
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.body,
+          track: null
+        };
+      }
+    },
+  },
   Track: {
     author: async ({ authorId }, _, { dataSources }) => {
       //   const baseUrl = 'https://odyssey-lift-off-rest-api.herokuapp.com';
@@ -23,7 +44,7 @@ const resolvers = {
       return dataSources.trackAPI.getAuthor(authorId);
     },
 
-    modules: ({id}, _, {dataSources}) => {
+    modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
   },
--- server/src/schema.js
+++ server/src/schema.js
@@ -11,6 +11,21 @@ const typeDefs = gql`
     track(id: ID!): Track
   }
 
+  type Mutation {
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
+  }
+
+  type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
+  }
+
   """
   A track is group of module
   """
